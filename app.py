9095e8c02dd4ea65938c8e0e99923091b60b9bae
Marcelo F Andrade <mfandrade@gmail.com>
2021-08-16 07:36:20 -0300

    first use of begin and end for cut_video in transcribe

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request, render_template
+from flask import Flask, redirect, request, render_template
 from werkzeug.utils import secure_filename
 from transcribe import transcribe
 
@@ -14,19 +14,30 @@ def hello():
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+
     transcription = ''
     info = ''
+
     if request.method == 'POST':
-        if 'filefield' in request.files:
+        if not 'filefield' in request.files:
+            redirect(request.url)
+
+        else:
             f = request.files['filefield']
-            f.save(secure_filename(f.filename))
-            transcription = transcribe(f.filename)
-            info = f'Arquivo: {f.filename}'
+            filename = secure_filename(f.filename)
+            f.save(filename)
+
+            info = f'Arquivo: {filename}'
+
+        begin = 0.0
+        end = -1.0
         if 'begin' in request.form and 'end' in request.form:
-            begin = request.form['begin']
-            end = request.form['end']
-            if len(begin) > 0 and len(end) > 0:
-                info += f' (trecho: {begin}-{end})'
+            begin = abs(min(float(request.form['begin'], float(request.form['end']))))
+            end = abs(max(float(request.form['begin'], float(request.form['end']))))
+            
+            info = info + f' (trecho: {begin} a {end})'
+
+        transcription = transcribe(filename, begin=begin, end=end)
 
     return render_template('trt8.html', transcription=transcription, info=info)
 
